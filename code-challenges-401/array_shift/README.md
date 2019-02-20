33dee9e6bca5820563d9f3c5b57808f4391072ed
Jasmin <jasmin.arensdorf@gmail.com>
2019-02-19 22:18:48 -0800

    Added docstring and comments.  Updated readme file.

--- code-challenges-401/array_shift/README.md
+++ code-challenges-401/array_shift/README.md
@@ -1,12 +1,17 @@
 # Array Shift
-# This program takes an input array and a value and inserts that value in the middle of the array.
+# This program takes an input array and a value and inserts that value in the middle 
+# of the array.
 
 ## Challenge
-# Find the middle of the input array for odd and even elements in the input array
+# Find the middle of the input array for odd and even elements and insert input value.      
 
 ## Approach & Efficiency
-
+# Create a new array containing first half of input array, the input value, and the last 
+# half of input array.  
 <!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
 
+
 ## Solution
+![alt text](http://array_shift.pdf)
+
 <!-- Embedded whiteboard image -->
--- code-challenges-401/array_shift/array_shift_module.py
+++ code-challenges-401/array_shift/array_shift_module.py
@@ -1,8 +1,25 @@
+# This program takes an input array and an input value and inserts that value in the middle 
+# of the array.  A new array is used to create this array.
 def insertShiftArray(original_arr, val):
+  """
+    Summary of insertShiftArray function:  takes an input array and an input value.  The input
+    value is inserted in the middle of the array.  A new array is used to create this array.
+
+    Parameters:
+    original_array (array): An array of values
+    val (int): An integer value
+    
+    Returns:
+    new_array (array): Returns a new array containing the the original array with the input value
+    inserted in the middle.
+  """
+
+  # Find the middle of the input array for odd and even elements       
   middle_position = (len(original_arr) // 2)
-  print('Middle position: ' + str(middle_position))
   new_arr = []
 
+  # Create a new array containing first half of input array, the input value, and the last half 
+  # of input array.
   if (len(original_arr) % 2 == 0):
     for i in range(middle_position):
       new_arr.append(original_arr[i])
@@ -10,9 +27,12 @@ def insertShiftArray(original_arr, val):
     for i in range(0, middle_position + 1):
       new_arr.append(original_arr[i])
 
+  # insert input value
   new_arr.append(val)
 
+  # append remaining input array elements
   for i in range( len(new_arr), len(original_arr) + 1):
     new_arr.append(original_arr[i - 1])
 
+  # return new array
   return new_arr
