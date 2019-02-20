c9e1633e78bcada6ff5480ceb6d1df769f2827d7
Jasmin <jasmin.arensdorf@gmail.com>
2019-02-20 15:22:27 -0800

    Added code to handle when value is not found in input array.

--- code-challenges-401/array_binary_search/array_binary_search_module.py
+++ code-challenges-401/array_binary_search/array_binary_search_module.py
@@ -7,17 +7,10 @@ def binary_search(arr, val):
     end = len(arr) - 1
     found = False
 
-    print('Arr: ' + str(arr))
-
-    while (found == False):
+    while (found == False) and (start <= end):
         # import pdb; pdb.set_trace()
         middle_index = (start + end) // 2
 
-        # print('Middle index: ' + str(middle_index))
-        
-        if (middle_index == -1):
-            # test condition when array is empty.  Value of middle_index will be -1
-            break
         if (val == arr[middle_index]):
             index = middle_index
             found = True
@@ -27,13 +20,4 @@ def binary_search(arr, val):
             else:
                 start = middle_index + 1
         
-        # print('Value: ' + str(middle_index))
-
     return index
-
-# input_array = []
-# value = 2
-# print('Input array: ' + str(input_array) + ' Value: ' + str(value))
-# # print
-# result = binary_search(input_array, 2)
-# print(str(result))
--- code-challenges-401/array_binary_search/array_binary_search_test.py
+++ code-challenges-401/array_binary_search/array_binary_search_test.py
@@ -4,24 +4,26 @@
 from  array_binary_search_module import binary_search
 
 def test_array_binary_search_even_array():
-    arr = [4, 8, 15, 16, 23, 42]
-    val = 15
-    # arr = [1, 2, 3, 4, 5, 6]
-    # val = 5
+    arr = [1, 2, 3, 4, 5, 6]
+    val = 5
     
     actual = binary_search(arr, val)
-    # expected = 4
-    expected = 2
+    expected = 4
     assert expected == actual
 
 def test_array_binary_search_odd_array():
-    # arr = [1, 2, 3, 5, 6]
-    # val = 2
+    arr = [1, 2, 3, 5, 6]
+    val = 2
+    
+    actual = binary_search(arr, val)
+    expected = 1
+    assert expected == actual
+
+def test_array_binary_search_value_not_found():
     arr = [11, 22, 33, 44, 55, 66, 77]
     val = 90
     
     actual = binary_search(arr, val)
-    # expected = 1
     expected = -1
     assert expected == actual
 
