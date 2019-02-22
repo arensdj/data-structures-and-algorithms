783e43a05e3f86901f3afeca558103f97f9c90b1
Jasmin <jasmin.arensdorf@gmail.com>
2019-02-22 14:34:36 -0800

    Coded includes, insert and print methods in LinkedList class.  Added a Node class.

--- code-challenges-401/linked_list/linked_list.py
+++ code-challenges-401/linked_list/linked_list.py
@@ -1,2 +1,54 @@
 class LinkedList():
-  pass
+	# pass
+
+	head = None
+
+	def insert(self, value):
+		#pass
+		node = Node(value)
+
+		# when head is not assigned, assign a node to it
+		if not self.head:
+			self.head = node
+		else:
+			current = self.head
+
+			# the head node does reference another node.  Find end of list and insert
+			while current._next:
+				current = current._next
+			
+			current._next = node
+
+		# print(str(current))
+
+	def includes(self, value):
+		current = self.head
+
+		# traverse linked list and fine value
+		while current:
+			if current.value == value:
+				return True
+
+			# set current to point to the next node
+			current = current._next
+
+		return False
+	
+	def print(self):
+		current = self.head
+		output = ''
+
+		while current:
+			output += current.value + ','
+			current = current._next
+		
+		return output
+
+class Node():
+	# constructor
+	def __init__(self, value):
+		self.value = value
+		self._next = None
+
+	
+
--- code-challenges-401/linked_list/test_linked_list.py
+++ code-challenges-401/linked_list/test_linked_list.py
@@ -1,5 +1,95 @@
-from linked_list import linked_list
+from linked_list import LinkedList
+
+"""
+# Can successfully instantiate an empty linked list
+# Can properly insert into the linked list
+# The head property will properly point to the first node in the linked list
+# Can properly  insert multiple nodes into the linked list
+# Will return true when finding a value within the linked list that exists
+# Will return false when searching for a value in the linked list that does not exist
+# Can properly return a collection of all the values that exist in the linked list
+"""
 
 def test_exists():
-  assert linked_list
+    """
+    """
+    assert LinkedList
+
+def test_instantiation():
+    """
+    Can successfully instantiate an empty linked list
+    """
+    assert LinkedList()
+
+
+def test_insert():
+    """
+    Can properly insert into the linked list
+    """
+    flowers = LinkedList()
+    flowers.insert('zinnia')
+
+    expected = 'zinnia'
+    actual = flowers.head.value
+
+    assert expected == actual
+
+def test_head_points_to_first_node():
+    """
+    The head property will properly point to the first node in the linked list
+    """
+    flowers = LinkedList()
+    flowers.insert('lavender')
+
+    expected = 'lavender'
+    actual = flowers.head.value
+
+    assert expected == actual
+
+
+def test_multiple_insert():
+    """
+    Can properly insert multiple nodes into the linked list
+    """
+    flowers = LinkedList()
+    flowers.insert('zinnia')
+    flowers.insert('lavender')
+    flowers.insert('rose')
+    
+    assert flowers.head.value == 'zinnia'
+    assert flowers.head._next.value == 'lavender'
+    assert flowers.head._next._next.value == 'rose'
+
+def test_includes():
+    """
+    Will return true when finding a value within the linked list that exists
+    """
+    flowers = LinkedList()
+    flowers.insert('zinnia')
+    flowers.insert('lavender')
+    flowers.insert('rose')
+
+    assert flowers.includes('rose')
+
+def test_not_includes():
+    """
+    Will return false when finding a value that doesn't exist in the linked list
+    """
+    flowers = LinkedList()
+    flowers.insert('zinnia')
+    flowers.insert('lavender')
+    flowers.insert('rose')
+
+    assert not flowers.includes('daisy')    
+
+def test_print_list():
+    """
+    Can properly return a collection of all the values that exist in the linked list
+    """
+    flowers = LinkedList()
+    flowers.insert('tulip')
+    flowers.insert('daffodile')
+    flowers.insert('crocus')
+
+    assert flowers.print() == 'tulip,daffodile,crocus,'
 
