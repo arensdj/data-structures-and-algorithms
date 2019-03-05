b98bf7bed11f42a03857c4b8a57a2917d3219db4
Jasmin <jasmin.arensdorf@gmail.com>
2019-03-05 06:12:07 -0800

    Coding enqueue.

--- code-challenges-401/queue_with_stacks/queue_with_stacks.py
+++ code-challenges-401/queue_with_stacks/queue_with_stacks.py
@@ -1,12 +1,12 @@
 from stacks_and_queues import Stack
-from linked_lists import Stack
+from linked_lists import LinkedList
 
 class PseudoQueue():
     def __init__(self):
         self._in_stack = Stack()
         self._out_stack = Stack()
-        self.front = None
-        self.rear = None
+        self.front = self._in_stack.top
+        self.rear = self._in_stack.top
 
         """
         only 2 Stacks available for storage
@@ -14,11 +14,22 @@ class PseudoQueue():
         def enqueue(self, val):
             """
             add to rear
-            """"
-            
+            """
+            # for item in range(len(input_list)):
+            #     self._in_stack.push(item)
+            self._in_stack.push(val)
+            self.front = self._in_stack.top
 
         def dequeue(self):
             """
             remove from front
             """
             pass
+
+def __init__ = "__main__":
+    card_queue = PseudoQueue()
+    card_list = ['1', '2', '3']
+    for item in range(len(card_list)):
+        card_queue.enqueue(item)
+    
+    print(str(card_queue))
