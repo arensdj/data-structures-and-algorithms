89880c7fee3df36b83f74a6206b8bfd763281399
Jasmin <jasmin.arensdorf@gmail.com>
2019-03-09 14:46:10 -0800

    Coding more functionality for animal shelter class.

--- code-challenges-401/fifo_animal_shelter/fifo_animal_shelter.py
+++ code-challenges-401/fifo_animal_shelter/fifo_animal_shelter.py
@@ -4,7 +4,9 @@ from linked_list import LinkedList
 class AnimalShelter():
     def __init__(self):
         self._in_stack = Stack()
-        self.front = self._in_stack.top
+        self._out_stack = Stack()
+        self.front_in_stack = self._in_stack.top
+        self.front_out_stack = self._out_stack.top
 
     def enqueue(self, animal):
         """
@@ -13,34 +15,30 @@ class AnimalShelter():
         # for item in range(len(input_list)):
         #     self._in_stack.push(item)
         self._in_stack.push(animal)
-        self.front = self._in_stack.top
-        print(str(self.front.value))
+        self.front_in_stack = self._in_stack.top
+        print('Front of in stack: ' + str(self.front.value))
 
     def dequeue(self, pref):
         """
         remove from top
         """
-        temp = ''
         if pref != 'dog' and pref != 'cat':
             return Null
         
         if self._out_stack.peek():
             return self._out_stack.pop()
 
-        # if self._in_stack.peek():
-        #     while self._in_stack.peek():
-        #         # temp = self._in_stack.pop()
-        #         # self._out_stack.push(temp)
-        #         self._out_stack.push(self._in_stack.pop())
+        if self._in_stack.peek():
+            while self._in_stack.peek():
+                self._out_stack.push(self._in_stack.pop())
 
-        #     # temp._next = None
-        #     self.front = self._in_stack.top
-        #     print(str(self.front.value))
-        #     print(str(temp.value))
+            self.front_out_stack = self._in_stack.top
+            print('Front out stack: ' + str(self.front_out_stack))
+            # print(str(temp.value))
 
-        #     return self._out_stack.pop()
-        # else:
-        #     return None
+            return self._out_stack.pop()
+        else:
+            return None
 
 
 if __name__ == "__main__":
@@ -50,5 +48,5 @@ if __name__ == "__main__":
     for animal in range(len(animals)):
         animal_queue.enqueue(animal)
     
-    animal_queue = AnimalShelter()
-    print('Front: ' + str(card_queue.front))
+    animal_queue.dequeue()
+    print('Front: ' + str(animal_queue.front))
