07d7076a1f830e7fa837f5c57cbdad0b4f68cb44
Jasmin <jasmin.arensdorf@gmail.com>
2019-03-12 08:17:37 -0700

    Initial scaffold.

---
+++ code-challenges-401/fizz_buzz_tree/fizz_buzz_tree.py
@@ -0,0 +1,20 @@
+from tree import Node, BinaryTree, BinaySearchTree
+
+
+def FizzBuzzTree(binary_tree):
+
+
+
+
+
+if __init__ = '__main__':
+    binary_search_tree = BinarySearchTree()
+    binary_search_tree.add(24)
+    binary_search_tree.add(5)
+    binary_search_tree.add(3)
+    binary_search_tree.add(10)
+    binary_search_tree.add(27)
+    binary_search_tree.add(182)
+
+    result = False
+    result = FizzBuzzTree(binary_search_tree)
